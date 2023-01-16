446fdc487884c9b0c65432f43362b26b0e9886b1
aleksanderstoyanov <blackeder3@gmail.com>
2023-01-16 19:34:00 +0200

    Add Update test for TransportCompanyDAO (#9)

--- src/test/java/TransportCompanyDAOTest.java
+++ src/test/java/TransportCompanyDAOTest.java
@@ -1,17 +1,18 @@
 import dao.TransportCompanyDAO;
 import dao.VehicleDAO;
-import entity.TransportCompany;
-import entity.Vehicle;
+import entity.*;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.junit.jupiter.api.*;
 import repository.TransportCompanyRepository;
+import repository.TransportationRepository;
 import util.EntitySeeder;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -34,19 +35,26 @@ public class TransportCompanyDAOTest {
     }
     @AfterAll()
     public static void afterEach() {
-        entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS = 0;");
-        entityManager.createNativeQuery("TRUNCATE TABLE transport_company");
-        entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS = 1;");
+        if (entityManager.getTransaction().isActive()){
+            entityManager.getTransaction().commit();
+        }
+
+        entityManager.getTransaction().begin();
+        entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS = 0;").executeUpdate();
+        entityManager.getTransaction().commit();
+
+        entityManager.getTransaction().begin();
+        entityManager.createNativeQuery("TRUNCATE TABLE transport_company").executeUpdate();
         entityManager.getTransaction().commit();
     }
     @Test
     @Order(1)
     public void createShouldExecuteSuccessfully(){
         //Arrange
-        String selectVehicleQuery = "SELECT * FROM transport_company AS tp WHERE tp.name = 'TestName'";
+        String selectedTransportCompanyQuery = "SELECT * FROM transport_company AS tp WHERE tp.name = 'TestName'";
 
         ArrayList<TransportCompany> resultList = (ArrayList<TransportCompany>) entityManager
-                .createNativeQuery(selectVehicleQuery, TransportCompany.class)
+                .createNativeQuery(selectedTransportCompanyQuery, TransportCompany.class)
                 .getResultList();
         //Act
         if (resultList.size() == 0) {
@@ -54,10 +62,37 @@ public class TransportCompanyDAOTest {
             entityManager.getTransaction().commit();
         }
         resultList = (ArrayList<TransportCompany>) entityManager
-                .createNativeQuery(selectVehicleQuery, TransportCompany.class)
+                .createNativeQuery(selectedTransportCompanyQuery, TransportCompany.class)
                 .getResultList();
 
         // Assert
+        int actual = Comparator.comparing(TransportCompany::getName)
+                .thenComparing(TransportCompany::getTotalIncome)
+                .thenComparing(TransportCompany::getAddress)
+                .compare(testTransportCompany, resultList.get(0));
+
+        assertEquals(0, 0);
+    }
+    @Test
+    @Order(2)
+    public void updateShouldExecuteSuccessfully(){
+        //Arrange
+        String selectedTransportCompanyQuery = "SELECT * FROM transport_company AS tp WHERE tp.name = 'UpdatedName'";
+        EntitySeeder.seedRecords(TransportCompanyRepository.transportCompanies);
+        testTransportCompany.setName("UpdatedName");
+        testTransportCompany.setTotalIncome(BigDecimal.TEN);
+        testTransportCompany.setAddress("UpdatedAddress");
+        testTransportCompany.setClients(new HashSet<Client>());
+        testTransportCompany.setEmployees(new HashSet<Employee>());
+        testTransportCompany.setVehicles(new HashSet<Vehicle>());
+        testTransportCompany.setTransportations(new HashSet<Transportation>());
+
+        //Act
+        transportCompanyDAO.update(1,testTransportCompany);
+        ArrayList<TransportCompany> resultList = (ArrayList<TransportCompany>) entityManager
+                .createNativeQuery(selectedTransportCompanyQuery, TransportCompany.class)
+                .getResultList();
+        //Assert
         int actual = Comparator.comparing(TransportCompany::getName)
                 .thenComparing(TransportCompany::getTotalIncome)
                 .thenComparing(TransportCompany::getAddress)
