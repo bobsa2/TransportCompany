c2e9adc282d64cd5bb955e6b2a82946109270396
aleksanderstoyanov <blackeder3@gmail.com>
2023-01-29 14:04:38 +0200

    Add Messages utility class (#18)

---
+++ src/main/java/util/Messages.java
@@ -0,0 +1,7 @@
+package util;
+
+public class Messages {
+   public static final String NOT_NULL_MESSAGE = " should not be empty!";
+   public static final String VALID_SIZE_MESSAGE = " should be in valid range!";
+   public static final String VALID_PATTERN_MESSAGE = " should be with a valid pattern!";
+}
